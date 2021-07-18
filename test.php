e6f6f4df14d0554223beb0fa2a9062a3cebe308f
Oleg Tishkin <umkasanki@gmail.com>
2021-07-18 13:49:13 +0300

    [3.7][Task] Add test of exception in getNextActions() method

--- test.php
+++ test.php
@@ -20,7 +20,8 @@ $addAction = new AddAction();
 //var_dump($task->getStatuses());
 //var_dump($sustomer->role);
 //var_dump($task->status);
-var_dump($task->getNextActions($sustomer));
+//var_dump($task->getNextActions('customer'));
+var_dump($task->getNextActions('developer'));
 //var_dump($task->getNextStatus('action_cancel'));
 
 //var_dump($task->getActions());
